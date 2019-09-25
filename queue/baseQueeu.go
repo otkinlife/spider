f646ddda78778440e57afb92c4c15c2febe642e8
kaichao1 <kaichao1@staff.weibo.com>
2019-09-25 10:33:44 +0800

    队列

--- queue/baseQueeu.go
+++ queue/baseQueeu.go
@@ -3,10 +3,44 @@ package queue
 import (
 	"../doSite"
 	"container/list"
+	"errors"
 )
 
-var q list.List
+type Queue struct {
+	l        *list.List
+	limitLen int
+}
 
-func PutIn(obj doSite.Site, target) {
+//初始化队列
+func New() *Queue {
+	return &Queue{
+		l:        list.New(),
+		limitLen: 0,
+	}
+}
 
+//入队列
+func (q *Queue) PutIn(obj doSite.SiteDownload) error {
+	if q.l.Len() >= q.limitLen {
+		err := errors.New("队列已满")
+		return err
+	}
+	e := list.Element{
+		Value: obj,
+	}
+	q.l.PushBack(e)
+	return nil
+}
+
+//出队列
+func (q *Queue) PutOut() doSite.SiteDownload {
+	if q.l.Len() == 0 {
+		return nil
+	}
+	outRes := q.l.Front()
+	q.l.Remove(q.l.Front())
+	if v, ok := outRes.Value.(doSite.SiteDownload); ok {
+		return v
+	}
+	return nil
 }
